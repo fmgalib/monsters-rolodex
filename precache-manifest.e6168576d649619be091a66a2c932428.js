794d9cf25ab420bb9934ca4d59dc39f966f1b51a
fmgalib <fmgalib@gmail.com>
2020-10-14 15:45:16 +0600

    Updates

---
+++ precache-manifest.e6168576d649619be091a66a2c932428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7184eb1cbb668d61a83d96a42a09a5df",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "618e3edbe52fd4748cec",
+    "url": "/monsters-rolodex/static/css/main.92935c24.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618e3edbe52fd4748cec",
+    "url": "/monsters-rolodex/static/js/main.dc39350b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
